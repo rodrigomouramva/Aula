d77ca96c2019fc565a1113e6b3ea0625d021b5f7
Rodrigo Tadeu Alves de Moura <rodrigomoura.mva@outlook.com>
2017-06-28 00:16:09 -0300

    Criação de repository e contratos

---
+++ RM.Aula.Domain/Interfaces/IEstadoRepository.cs
@@ -0,0 +1,13 @@
+﻿using RM.Aula.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RM.Aula.Domain.Interfaces
+{
+    public interface IEstadoRepository : IRepository<Estado>
+    {
+    }
+}
